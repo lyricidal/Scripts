6d02a2071be73e5fb8c8c7c79bca7e81d40ba657
lyricidal <lyricidal@gmail.com>
2019-10-02 01:37:27 -0400

    Minor update Install.sh

--- Masternodes/Install.sh
+++ Masternodes/Install.sh
@@ -1,11 +1,11 @@
 clear
 echo "Starting DAPS Masternode download and install..."
 echo "Updating/Upgrading OS..."
-sudo apt update && sudo apt -y upgrade
+sudo apt update && sudo apt upgrade -y
 echo "Downloading latest build..."
 wget https://github.com/DAPSCoin/DAPSCoin/releases/download/1.0/dapscoin_linux-v1.0.0.zip
-echo "Installing unzip"
-sudo apt-get install unzip
+echo "Installing unzip..."
+sudo apt-get install unzip -y
 echo "Unzipping dapscoin_linux-v1.0.0.zip..."
 sudo unzip dapscoin_linux-v1.0.0.zip -d /usr/local/bin
 chmod +x /usr/local/bin/dapscoind
@@ -16,5 +16,5 @@ mkdir ~/.dapscoin
 cd ~/.dapscoin
 echo "Editing dapscoin.conf..."
 vi dapscoin.conf
-echo "Launching dapscoind"
+echo "Launching dapscoind..."
 dapscoind -daemon
