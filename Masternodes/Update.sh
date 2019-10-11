5f211f67d8350438429e9c072d7422d287d7cde0
lyricidal <lyricidal@gmail.com>
2019-10-11 13:13:02 -0400

    Create Update.sh

---
+++ Masternodes/Update.sh
@@ -0,0 +1,11 @@
+echo "Stopping dapscoind..."
+dapscoin-cli stop
+echo "Downloading update..."
+wget https://github.com/DAPSCoin/DAPSCoin/releases/download/1.0.1/master_linux-v1.0.1.3.zip
+echo "Extracting update..."
+sudo unzip -jo master_linux-v1.0.1.3.zip -d /usr/local/bin
+echo "Set permissions on files..."
+chmod +x /usr/local/bin/daps*
+echo "Running dapscoind..."
+dapscoind -daemon
+echo "Update completed!"
