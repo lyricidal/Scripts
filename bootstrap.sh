62678bd4407b5135d46e5407ef3e28be4afcab72
lyricidal <lyricidal@gmail.com>
2019-10-12 11:00:29 -0400

    Mislabeled step

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,6 +4,6 @@ echo "Downloading bootstrap..."
 wget https://github.com/DAPSCoin/DAPSCoin/releases/download/1.0.1/bootstrap.zip
 echo "Removing old blocks folder and peers.dat..."
 rm -rf ~/.dapscoin/blocks ~/.dapscoin/peers.dat
-echo "Removing old blocks folder and peers.dat..."
+echo "Installing new blocks folder and peers.dat..."
 unzip bootstrap.zip -d ~/.dapscoin
 echo "Bootstrap installed!"
