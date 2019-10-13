4d2b5a2d1173f72785d061f4a99dbcc5c3f8acda
lyricidal <lyricidal@gmail.com>
2019-10-12 23:41:51 -0400

    Remove error causing folders

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,8 +2,8 @@ echo "Stopping dapscoind..."
 dapscoin-cli stop
 echo "Downloading bootstrap..."
 wget https://github.com/DAPSCoin/DAPSCoin/releases/download/1.0.1/bootstrap.zip
-echo "Removing old blocks folder and peers.dat..."
-rm -rf ~/.dapscoin/blocks ~/.dapscoin/peers.dat
+echo "Removing old blocks, chainstate, database, and peers.dat..."
+rm -rf ~/.dapscoin/blocks ~/.dapscoin/chainstate ~/.dapscoin/database ~/.dapscoin/peers.dat
 echo "Installing new blocks folder and peers.dat..."
 unzip bootstrap.zip -d ~/.dapscoin
 echo "Bootstrap installed!"
