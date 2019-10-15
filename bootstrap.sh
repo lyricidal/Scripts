f49eb68d1134634a0a165cad49c0618f996204cd
lyricidal <lyricidal@gmail.com>
2019-10-15 15:22:16 -0400

    Update link, remove peers.dat

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,9 @@
 echo "Stopping dapscoind..."
 dapscoin-cli stop
 echo "Downloading bootstrap..."
-wget https://github.com/DAPSCoin/DAPSCoin/releases/download/1.0.1/bootstrap.zip
-echo "Removing old blocks, chainstate, database, and peers.dat..."
-rm -rf ~/.dapscoin/blocks ~/.dapscoin/chainstate ~/.dapscoin/database ~/.dapscoin/peers.dat
-echo "Installing new blocks folder and peers.dat..."
+wget https://github.com/DAPSCoin/BootStrap/raw/master/bootstrap.zip
+echo "Removing old blocks, chainstate, and database folders...."
+rm -rf ~/.dapscoin/blocks ~/.dapscoin/chainstate ~/.dapscoin/database
+echo "Installing new blocks folders..."
 unzip bootstrap.zip -d ~/.dapscoin
 echo "Bootstrap installed!"
