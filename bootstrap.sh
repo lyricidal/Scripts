1c49376be7002b87a57a52796320ed3ba9deb9a9
lyricidal <lyricidal@gmail.com>
2019-10-16 20:25:21 -0400

    Update new, properly tagged "latest" release .zip

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
 echo "Stopping dapscoind..."
 dapscoin-cli stop
 echo "Downloading bootstrap..."
-wget https://github.com/DAPSCoin/BootStrap/raw/master/bootstrap.zip
+wget https://github.com/DAPSCoin/BootStrap/releases/download/latest/bootstrap.zip
 echo "Removing old blocks, chainstate, and database folders...."
 rm -rf ~/.dapscoin/blocks ~/.dapscoin/chainstate ~/.dapscoin/database
 echo "Installing new blocks folders..."
