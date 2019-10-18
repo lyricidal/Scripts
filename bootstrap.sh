1d60320b57af4b7e7356c0da8e01581a5e9d6cec
lyricidal <lyricidal@gmail.com>
2019-10-18 18:20:02 -0400

    Add sleep, -N to wget, remove .lock and dapscoind.pid

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,10 @@
 echo "Stopping dapscoind..."
 dapscoin-cli stop
+sleep 5
 echo "Downloading bootstrap..."
-wget https://github.com/DAPSCoin/BootStrap/releases/download/latest/bootstrap.zip
+wget -N https://github.com/DAPSCoin/BootStrap/releases/download/latest/bootstrap.zip
 echo "Removing old blocks, chainstate, and database folders...."
-rm -rf ~/.dapscoin/blocks ~/.dapscoin/chainstate ~/.dapscoin/database
+rm -rf ~/.dapscoin/blocks ~/.dapscoin/chainstate ~/.dapscoin/database ~/.dapscoin/.lock ~/.dapscoin/dapscoind.pid
 echo "Installing new blocks folders..."
 unzip bootstrap.zip -d ~/.dapscoin
 echo "Bootstrap installed!"
