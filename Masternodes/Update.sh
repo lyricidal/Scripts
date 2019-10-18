595446f416edae35a6eb6702db1f2438c1247d2a
lyricidal <lyricidal@gmail.com>
2019-10-18 19:47:50 -0400

    Add unzip to bootstrap.sh/Update.sh

--- Masternodes/Update.sh
+++ Masternodes/Update.sh
@@ -5,6 +5,8 @@ echo "Stopping dapscoind..."
 dapscoin-cli stop
 echo "Downloading update..."
 wget -N https://github.com/DAPSCoin/DAPSCoin/releases/download/1.0.2/master_linux-v1.0.2.zip
+echo "Installing unzip..."
+sudo apt-get install unzip -y
 echo "Extracting update..."
 sudo unzip -jo master_linux-v1.0.2.zip -d /usr/local/bin
 echo "Set permissions on files..."
--- bootstrap.sh
+++ bootstrap.sh
@@ -8,6 +8,8 @@ echo "Downloading bootstrap..."
 wget -N https://github.com/DAPSCoin/BootStrap/releases/download/latest/bootstrap.zip
 echo "Removing old blocks, chainstate, and database folders...."
 rm -rf ~/.dapscoin/blocks ~/.dapscoin/chainstate ~/.dapscoin/database ~/.dapscoin/.lock ~/.dapscoin/dapscoind.pid
+echo "Installing unzip..."
+sudo apt-get install unzip -y
 echo "Installing new blocks folders..."
 unzip bootstrap.zip -d ~/.dapscoin
 echo "Bootstrap installed!"
