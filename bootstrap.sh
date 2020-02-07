6b37b3f1f2c95d8ce29af3703169d86a821acd77
lyricidal <lyricidal@gmail.com>
2020-02-06 23:13:00 -0500

    Update bootstrap.sh for new .zip setup

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,15 +4,12 @@ echo "Starting DAPS Bootstrap install script"
 echo "Stopping dapscoind..."
 dapscoin-cli stop
 sleep 5
-echo "Downloading Blocks folder..."
+echo "Downloading BootStrap..."
 wget -N https://github.com/DAPSCoin/BootStrap/releases/download/latest/bootstrap.zip
-echo "Downloading Chainstate folder..."
-wget -N https://github.com/DAPSCoin/BootStrap/releases/download/latest/chainstate.zip
 echo "Removing old blocks, chainstate, and database folders...."
 rm -rf ~/.dapscoin/blocks ~/.dapscoin/chainstate ~/.dapscoin/database ~/.dapscoin/.lock ~/.dapscoin/dapscoind.pid
 echo "Installing unzip..."
 sudo apt-get install unzip -y
-echo "Installing new blocks folders..."
-unzip bootstrap.zip -d ~/.dapscoin
-unzip chainstate.zip -d ~/.dapscoin
+echo "Installing new blocks/chainstate folders..."
+unzip -o bootstrap.zip -d ~/.dapscoin
 echo "Bootstrap installed!"
