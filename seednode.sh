a44d218d5eb8bdac0a4d9c67a571c2a07a5eceda
lyricidal <lyricidal@gmail.com>
2020-03-14 12:06:58 -0400

    Update repo, add install to seednode.sh

--- seednode.sh
+++ seednode.sh
@@ -1,10 +1,15 @@
 cd
+echo "Checking for updates..."
 sudo apt update
+echo "Applying any available upgrades..."
 sudo apt upgrade -y
 echo "Installing dependencies..."
 sudo apt install build-essential libboost-dev libssl-dev -y
 echo "Cloning Git Repo..."
-git clone https://github.com/lyricidal/daps-seeder.git
+git clone https://github.com/DAPSCoin/daps-seeder.git
 echo "Building seeder..."
 cd daps-seeder
 make
+echo "Installing to /usr/local/bin..."
+cp dnsseed /usr/local/bin
+echo "Done!"
