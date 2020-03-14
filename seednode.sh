4efafe07a89287b088b0f2f7b79ea40a88b6a199
lyricidal <lyricidal@gmail.com>
2020-03-14 10:45:57 -0400

    Add build-essential package to seednode.sh

--- seednode.sh
+++ seednode.sh
@@ -2,7 +2,7 @@ cd
 sudo apt update
 sudo apt upgrade -y
 echo "Installing dependencies..."
-sudo apt install libboost-dev libssl-dev -y
+sudo apt install build-essential libboost-dev libssl-dev -y
 echo "Cloning Git Repo..."
 git clone https://github.com/lyricidal/daps-seeder.git
 echo "Building seeder..."
