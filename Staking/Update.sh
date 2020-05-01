4fcc2d33189b38665553de900d27bb1b4c51eb06
DAPS_Spock <55909478+DAPS-Spock@users.noreply.github.com>
2020-05-01 15:03:03 +1000

    Creating Update.sh for staking nodes

---
+++ Staking/Update.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+clear
+echo "Starting DAPS Staking update script"
+echo "Stopping dapscoind..."
+dapscoin-cli stop
+echo "Downloading update..."
+wget -N https://github.com/DAPSCoin/DAPSCoin/releases/download/1.0.6.5/dapscoin-v1.0.6.5-linux.zip
+echo "Installing unzip..."
+sudo apt-get install unzip -y
+echo "Extracting update..."
+sudo unzip -jo dapscoin-v1.0.6.5-linux.zip -d /usr/local/bin
+echo "Set permissions on files..."
+sudo chmod +x /usr/local/bin/daps*
+echo "Running dapscoind..."
+dapscoind -daemon
+echo "Cleaning up..."
+cd
+rm -rf dapscoin-v1.0.6.5-linux.zip
+echo "Update completed!"
