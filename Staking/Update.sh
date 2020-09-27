c4119b5c843fbad64b26c52800453a8804706b39
DAPS_Spock <55909478+DAPS-Spock@users.noreply.github.com>
2020-09-27 16:58:14 +1000

    Update Update.sh

--- Staking/Update.sh
+++ Staking/Update.sh
@@ -8,12 +8,12 @@ wget -N https://github.com/DAPSCoin/DAPSCoin/releases/download/1.0.8.0/dapscoin-
 echo "Installing unzip..."
 sudo apt-get install unzip -y
 echo "Extracting update..."
-sudo unzip -jo dapscoin-v1.0.7.1-linux.zip -d /usr/local/bin
+sudo unzip -jo dapscoin-v1.0.8.0-linux.zip -d /usr/local/bin
 echo "Set permissions on files..."
 sudo chmod +x /usr/local/bin/daps*
 echo "Running dapscoind..."
 dapscoind -daemon
 echo "Cleaning up..."
 cd
-rm -rf dapscoin-v1.0.7.1-linux.zip
+rm -rf dapscoin-v1.0.8.0-linux.zip
 echo "Update completed!"
