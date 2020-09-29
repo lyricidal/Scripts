a45ed66531a242ae79014ace349756dda3240204
lyricidal <lyricidal@gmail.com>
2020-09-29 17:33:32 -0400

    Add generic update script
    This works for stakers, masternodes, or PoA miners

---
+++ Update.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+VERSION=1.0.8.0
+
+clear
+echo "Starting DAPS update script"
+echo "Stopping dapscoind..."
+dapscoin-cli stop
+echo "Downloading update..."
+wget -N https://github.com/DAPSCoin/DAPSCoin/releases/download/$VERSION/dapscoin-v$VERSION-linux.zip
+echo "Installing unzip..."
+sudo apt-get install unzip -y
+echo "Extracting update..."
+sudo unzip -jo dapscoin-v$VERSION-linux.zip -d /usr/local/bin
+echo "Set permissions on files..."
+sudo chmod +x /usr/local/bin/daps*
+echo "Running dapscoind..."
+dapscoind -daemon
+echo "Cleaning up..."
+cd
+rm -rf dapscoin-v$VERSION-linux.zip
+echo "Update completed!"
